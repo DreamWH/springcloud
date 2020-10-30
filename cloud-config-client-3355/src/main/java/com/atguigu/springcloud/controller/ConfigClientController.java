2dd484425de49aa5cdc2140c6e0f4d28f058a7e3
wangjun <1150993274@qq.com>
2020-10-30 19:28:43 +0800

    spring cloud config 配置中心 客户端搭建完成 手动刷新

--- cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3355/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,17 +1,24 @@
 package com.atguigu.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigClientController {
 
-    @Value("${ruoyi.profile}")
+    @Value("${gbs.host}")
     private String file;
 
     @GetMapping("/file")
     private String fileName(){
         return file;
+    }
+
+    @GetMapping("/name")
+    private String testName(){
+        return "hello";
     }
 }
