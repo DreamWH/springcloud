3bd50fe04e6040d77908693511173887c77b82c1
wangjun <1150993274@qq.com>
2020-11-04 21:07:46 +0800

    spring cloud stream 消费者完成，8802,8803 重复消费问题，持久化问题·

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/ConsumerMessageMain8802.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerMessageMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerMessageMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/controller/ConsumerMessageController8803.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud.controller;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)
+public class ConsumerMessageController8803 {
+
+    @StreamListener(Sink.INPUT)
+    public void receive(String message) {
+        System.out.println("message = " + message);
+    }
+}
