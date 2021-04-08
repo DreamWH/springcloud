24aa9c7a8d40cedd4b724410e61912d43d738022
wangjun <1150993274@qq.com>
2021-04-08 19:30:18 +0800

    测试降级服务

--- cloudalibaba-sentinel-service-8041/src/main/java/com/atguigu/springcloud/controlle/SentinelServiceController.java
+++ cloudalibaba-sentinel-service-8041/src/main/java/com/atguigu/springcloud/controlle/SentinelServiceController.java
@@ -1,18 +1,28 @@
 package com.atguigu.springcloud.controlle;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class SentinelServiceController {
 
     @GetMapping("/getA")
+    @SentinelResource(value = "getResource",blockHandler = "handleException")
     public String testA(){
-        return "----testA";
+        return "----testA" + Thread.currentThread().getName();
     }
 
     @GetMapping("/getB")
     public String testB(){
-        return "----testB";
+        System.out.println(Thread.currentThread().getName());
+        int i = 1 / 0;
+        return "----testB" + Thread.currentThread().getName();
+    }
+
+    public String handleException(BlockException blockException){
+        return "handleException";
     }
 }
